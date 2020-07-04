18d3a27e0f9bd3396f97f46e90916270bf7db81f
Javier Alarcon <alarcon.vt@gmail.com>
2020-07-03 21:53:16 -0500

    inicio del proyecto

---
+++ lib/src/pages/login_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('esta es la pagina del login'),
+      ),
+    );
+  }
+}
