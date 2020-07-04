093f333e2e364ade2d9d2cc002435969901fdada
Javier Alarcon <alarcon.vt@gmail.com>
2020-07-03 23:06:36 -0500

    agregado bloc y provider con un innerwidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:formvalidation/src/bloc/provider.dart';
+
 import 'package:formvalidation/src/pages/home_page.dart';
 import 'package:formvalidation/src/pages/login_page.dart';
  
@@ -8,14 +10,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      initialRoute: 'login',
-      routes: {
-        'login' : (BuildContext context) => LoginPage(),
-        'home'  : (BuildContext context) => HomePage(),
-      },
+    return Provider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'login',
+        routes: {
+          'login' : (BuildContext context) => LoginPage(),
+          'home'  : (BuildContext context) => HomePage(),
+        },
+      )
     );
   }
 }
