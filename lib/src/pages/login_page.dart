6ca8640b69dc6294acb3792c937a94387f29ae88
Javier Alarcon <alarcon.vt@gmail.com>
2020-07-03 22:38:38 -0500

    agregado diseño formulario login

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -8,8 +8,102 @@ class LoginPage extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           _crearFondo(context),
+          _loginForm(context),
         ],
       ),
+    );
+  }
+
+  Widget _loginForm(BuildContext context){
+
+    final size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SafeArea(
+            child: Container(
+              height: size.height * 0.3,
+            )
+            
+          ),
+          Container(
+            width: size.width * 0.85,
+            padding: EdgeInsets.symmetric( vertical: 50.0 ),
+            margin: EdgeInsetsDirectional.only( bottom: 30.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular( 5.0 ),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 3.0,
+                  offset: Offset(0.0 , 5.0),
+                  spreadRadius: 3.0,
+                )
+              ]
+            ),
+            child: Column(
+              children: <Widget>[
+                Text('Ingreso', style:TextStyle( fontSize: 20.0 )),
+                SizedBox( height: 30.0 ),
+                _crearEmail(),
+                SizedBox( height: 15.0 ),
+                _crearPassword(),
+                SizedBox( height: 20.0 ),
+                _crearButton(),
+              ],
+            ),
+          ),
+
+          Text('¿Olvido la contraseña?'),
+          SizedBox( height: 50.0 )
+
+        ],
+      ),
+    );
+  }
+
+  Widget _crearEmail(){
+    return Container(
+      padding: EdgeInsets.symmetric( horizontal: 20.0),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon( Icons.alternate_email, color: Colors.deepPurple ),
+          hintText: 'ejemplo@correo.com',
+          labelText: 'Correo electrónico',
+        ),
+      ),
+    );
+  }
+
+  Widget _crearPassword(){
+    return Container(
+      padding: EdgeInsets.symmetric( horizontal: 20.0),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon( Icons.lock_outline, color: Colors.deepPurple ),
+          labelText: 'Contraseña',
+        ),
+      ),
+    );
+  }
+
+  Widget _crearButton(){
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric( horizontal: 80.0, vertical: 15.0),
+        child: Text('Ingresar'),
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular( 5.0 ),
+      ),
+      elevation: 0.0,
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      onPressed: (){}
     );
   }
 
@@ -48,7 +142,7 @@ class LoginPage extends StatelessWidget {
         Positioned( bottom: 120.0, right: 20.0, child: circulo, ),
 
         Container(
-          padding: EdgeInsets.only(top: 80.0),
+          padding: EdgeInsets.only(top: size.height * 0.1),
           child: Column(            
             children: <Widget>[
               Icon( Icons.person_pin_circle, color: Colors.white, size: 100.0),
