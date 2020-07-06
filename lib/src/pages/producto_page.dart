586838a62558837b76ee2ca6b0344786ec940782
Javier Alarcon <alarcon.vt@gmail.com>
2020-07-05 20:29:16 -0500

    agregado vista del productos page y validacion de formularios

--- lib/src/pages/producto_page.dart
+++ lib/src/pages/producto_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:formvalidation/src/utils/utils.dart' as utils;
 
-class ProductoPage extends StatelessWidget {
-  
+class ProductoPage extends StatefulWidget {
+
+  @override
+  _ProductoPageState createState() => _ProductoPageState();
+}
+
+class _ProductoPageState extends State<ProductoPage> {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +29,72 @@ class ProductoPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Container(
           padding: EdgeInsets.all(15.0),
+          child: _crearFormulario()
         ),
       ),
     );
+  }
+
+  Widget _crearFormulario(){
+    return Form(
+      key: formKey,
+      child: Column(
+        children: <Widget>[
+          _crearNombre(),
+          _crearPrecio(),
+          _crearBoton()
+        ],  
+      ),
+    );
+  }
+
+  Widget _crearNombre(){
+    return TextFormField(
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        labelText: 'Producto',
+      ),
+      validator: ( value ){
+        if( value.length < 3 ){
+          return 'Ingrese el nombre del producto';
+        }else
+          return null;
+      },
+    );
+  }
+
+  Widget _crearPrecio(){
+    return TextFormField(
+      keyboardType: TextInputType.numberWithOptions( decimal: true ),
+      decoration: InputDecoration(
+        labelText: 'Precio',
+      ),
+      validator: ( value ){
+        if( utils.isNumeric( value ) ){
+          return null;
+        }else{
+          return 'Sólo números';
+        }
+      },
+    );
+  }
+
+  Widget _crearBoton(){
+    return RaisedButton.icon(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      color:Colors.deepPurple,
+      textColor: Colors.white,
+      label: Text('Guardar'),
+      icon: Icon( Icons.save ) ,
+      onPressed: _submit
+    );
+  }
+
+  void _submit(){    
+    if(!formKey.currentState.validate()) return;
+
+    print('todo ok');
   }
 }
